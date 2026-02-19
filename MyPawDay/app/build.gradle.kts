c54e8ed5a1e8f64a994922a5bb25c1b57edeb806
souk0712 <souk0712@gmail.com>
2026-02-19 18:51:18 +0900

    config : 라이브러리 정리 및 버전 업그레이드 (#32)

--- MyPawDay/app/build.gradle.kts
+++ MyPawDay/app/build.gradle.kts
@@ -44,13 +44,8 @@ android {
 
 dependencies {
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
+    implementation(libs.bundles.androidx)
     implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.bundles.api)
     implementation(libs.hilt.android)
