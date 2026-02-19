e198582bb599f476b9eaeba6502f219c1a66e267
souk0712 <souk0712@gmail.com>
2026-02-19 18:34:04 +0900

    config : 라이브러리 추가 (#32)

--- MyPawDay/app/build.gradle.kts
+++ MyPawDay/app/build.gradle.kts
@@ -2,11 +2,14 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
     namespace = "com.tico.mypawday"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.tico.mypawday"
@@ -49,6 +52,12 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.bundles.api)
+    implementation (libs.hilt.android)
+    ksp(libs.hilt.compiler)
+    implementation(libs.glide.compose)
+    implementation(libs.androidx.hilt.navigation.compose)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
