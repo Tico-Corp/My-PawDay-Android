11ed139b67ba916c5ea7efd727a5606fa3a0c8af
souk0712 <souk0712@gmail.com>
2026-02-24 07:36:12 +0900

    feat : 색상을 테마에 연결 (#3)

--- MyPawDay/app/src/main/java/com/tico/mypawday/ui/theme/Theme.kt
+++ MyPawDay/app/src/main/java/com/tico/mypawday/ui/theme/Theme.kt
@@ -1,16 +1,15 @@
 package com.tico.mypawday.ui.theme
 
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 
 private val LightColorScheme = lightColorScheme(
     primary = lightPrimary,
@@ -273,6 +272,8 @@ private val LightCustomColors = CustomColorScheme(
     yellowShinyGold = lightYellowShinyGold,
 )
 
+val LocalCustomColorScheme = staticCompositionLocalOf { CustomColorScheme() }
+
 private val DarkCustomColors = CustomColorScheme(
     gray10 = darkGray10,
     gray20 = darkGray20,
@@ -289,21 +290,22 @@ private val DarkCustomColors = CustomColorScheme(
 @Composable
 fun MyPawDayTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    dynamicColor: Boolean = false,
     content: @Composable () -> Unit,
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
+    val colorScheme = if (darkTheme) DarkColorScheme else LightColorScheme
+    val customColorScheme = if (darkTheme) DarkCustomColors else LightCustomColors
 
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+    CompositionLocalProvider(LocalCustomColorScheme provides customColorScheme) {
+        MaterialTheme(
+            colorScheme = colorScheme,
+            content = content
+        )
     }
+}
 
-    MaterialTheme(
-        colorScheme = colorScheme,
-        content = content
-    )
+object MyPawDayTheme {
+    val colorScheme: CustomColorScheme
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalCustomColorScheme.current
 }
