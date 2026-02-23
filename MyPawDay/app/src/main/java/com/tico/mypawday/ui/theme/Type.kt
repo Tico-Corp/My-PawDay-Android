8c875f61f3bad883f3b0f7b0f4c26ce803e7f6ae
souk0712 <souk0712@gmail.com>
2026-02-24 05:24:43 +0900

    asset : 사용하는 폰트 추가 (#2)

--- MyPawDay/app/src/main/java/com/tico/mypawday/ui/theme/Type.kt
+++ MyPawDay/app/src/main/java/com/tico/mypawday/ui/theme/Type.kt
@@ -1,34 +1,148 @@
 package com.tico.mypawday.ui.theme
 
-import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.tico.mypawday.R
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+val RobotoFontFamily = FontFamily(
+    Font(R.font.roboto_regular, FontWeight.Normal),
+    Font(R.font.roboto_bold, FontWeight.Bold),
+)
+
+object MyPawDayTypography {
+
+    val robotoRegular10 = TextStyle(
+        fontFamily = RobotoFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 10.sp,
+    )
+
+    val robotoRegular12 = TextStyle(
+        fontFamily = RobotoFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+    )
+
+    val robotoRegular14 = TextStyle(
+        fontFamily = RobotoFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+    )
+
+    val robotoRegular16 = TextStyle(
+        fontFamily = RobotoFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
     )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+
+    val robotoRegular18 = TextStyle(
+        fontFamily = RobotoFontFamily,
         fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        fontSize = 18.sp,
     )
-    */
-)
+
+    val robotoRegular20 = TextStyle(
+        fontFamily = RobotoFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 20.sp,
+    )
+
+    val robotoRegular24 = TextStyle(
+        fontFamily = RobotoFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 24.sp,
+    )
+
+    val robotoRegular28 = TextStyle(
+        fontFamily = RobotoFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 28.sp,
+    )
+
+    val robotoRegular30 = TextStyle(
+        fontFamily = RobotoFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 30.sp,
+    )
+
+    val robotoRegular32 = TextStyle(
+        fontFamily = RobotoFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 32.sp,
+    )
+
+    val robotoRegular48 = TextStyle(
+        fontFamily = RobotoFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 48.sp,
+    )
+
+    val robotoBold10 = TextStyle(
+        fontFamily = RobotoFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 10.sp,
+    )
+
+    val robotoBold12 = TextStyle(
+        fontFamily = RobotoFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 12.sp,
+    )
+
+    val robotoBold14 = TextStyle(
+        fontFamily = RobotoFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp,
+    )
+
+    val robotoBold16 = TextStyle(
+        fontFamily = RobotoFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 16.sp,
+    )
+
+    val robotoBold18 = TextStyle(
+        fontFamily = RobotoFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 18.sp,
+    )
+
+    val robotoBold20 = TextStyle(
+        fontFamily = RobotoFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp,
+    )
+
+    val robotoBold24 = TextStyle(
+        fontFamily = RobotoFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 24.sp,
+    )
+
+    val robotoBold28 = TextStyle(
+        fontFamily = RobotoFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 28.sp,
+    )
+
+    val robotoBold30 = TextStyle(
+        fontFamily = RobotoFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 30.sp,
+    )
+
+    val robotoBold32 = TextStyle(
+        fontFamily = RobotoFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 32.sp,
+    )
+
+    val robotoBold48 = TextStyle(
+        fontFamily = RobotoFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 48.sp,
+    )
+}
